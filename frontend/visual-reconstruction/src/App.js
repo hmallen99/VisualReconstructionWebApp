50d1cb875efd0474cbc15d93d4a5082acd9399bb
hmallen99 <henmanallen@berkeley.edu>
2020-11-02 16:08:49 -0800

    added frontend framework

--- frontend/visual-reconstruction/src/App.js
+++ frontend/visual-reconstruction/src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <nav>
+          <p>
+            <Link to="/">Home</Link>
+          </p>
+          <p>
+            <Link to="/demos">Demos</Link>
+          </p>
+          <p>
+            <Link to="/wiki">Wiki</Link>
+          </p>
+        </nav>
+
+        <Switch>
+          <Route path="/demos">
+            <Demos />
+          </Route>
+          <Route path="/wiki">
+            <Wiki />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>
+        Main Demo!
+      </h2>
+      <p>
+        Demo code here!
+      </p>
+    </div>
+  );
+}
+
+
+function Demos() {
+  return (
+    <div>
+      <h2>
+        Other Demos!
+      </h2>
+      <p>
+        Demos code here!
+      </p>
+    </div>
+  );
+}
+
+function Wiki() {
+  return (
+    <div>
+      <h2>
+        Wiki!
+      </h2>
+      <ul>
+        <li>
+          Human Vision
+        </li>
+        <li>
+          Computer Vision
+        </li>
+        <li>
+          Human-Computer Vision
+        </li>
+      </ul>
     </div>
   );
 }
