9d02a7bd1cd3bb7538deb082016ead3dc7ed6279
hmallen99 <henmanallen@berkeley.edu>
2020-11-02 16:46:04 -0800

    Added backend framework

--- backend/vis_backend.py
+++ backend/vis_backend.py
@@ -1,6 +1,17 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def vis_main():
     return "NT@B Visual Reconstruction"
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload_file():
+    #TODO: Get images from frontend here
+    return "Upload a file"
+
+@app.route('/generate3d', methods=['GET', 'POST'])
+def generate_3d():
+    #TODO: Send an obj file of with the 3D model to the frontend
+    return "Generate 3D image"
