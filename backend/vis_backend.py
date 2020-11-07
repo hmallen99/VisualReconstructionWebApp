fee36bf23c0a36e2aa4174c7334db2a6fc30e309
Kelly <kellyruanyu@gmail.com>
2020-11-07 00:24:53 -0800

    added image upload

--- backend/vis_backend.py
+++ backend/vis_backend.py
@@ -9,9 +9,13 @@ def vis_main():
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload_file():
     #TODO: Get images from frontend here
+    file = request.files['file']
     return "Upload a file"
 
 @app.route('/generate3d', methods=['GET', 'POST'])
 def generate_3d():
     #TODO: Send an obj file of with the 3D model to the frontend
     return "Generate 3D image"
+
+if __name__ == "__main__":
+    app.run()
--- frontend/visual-reconstruction/src/Pages/Demos.js
+++ frontend/visual-reconstruction/src/Pages/Demos.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
-function Demos() {
-  return (
-    <div className="Demos">
-      <h2>
-        Other Demos
-      </h2>
-      <p>
-        Demos code here!
-      </p>
-    </div>
-  );
+class Demos extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleUploadImage = this.handleUploadImage.bind(this);
+    }
+
+    handleUploadImage(e) {
+        e.preventDefault();
+
+        const data = new FormData();
+        data.append('file', this.uploadInput.files[0]);
+
+        fetch('http://localhost:5000/uploads', {
+            method: 'POST',
+            body: data,
+        });
+    }
+
+    render() {
+      return (
+        <div className="Demos">
+          <h2>
+            Other Demos
+          </h2>
+          <p>
+            Demos code here!
+            <form onSubmit={this.handleUploadImage}>
+                <div>
+                  <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+                </div>
+                <br />
+                <div>
+                  <button>Upload</button>
+                </div>
+            </form>
+          </p>
+        </div>
+      );
+    }
+
 }
 
 export default Demos;
